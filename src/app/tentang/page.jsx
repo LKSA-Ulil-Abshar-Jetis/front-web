578935ab4887008df519d28a2cb7d700a8ac2388
Muhammad Nizar <mnizarzr@gmail.com>
2024-02-08 22:28:36 +0700

    update tentang

--- src/app/tentang/page.jsx
+++ src/app/tentang/page.jsx
@@ -1,4 +1,4 @@
-import sejarah from "@/data/sejarah";
+import tentang from "@/data/tentang";
 import parse from "html-react-parser";
 
 export default function Sejarah() {
@@ -9,53 +9,28 @@ export default function Sejarah() {
           <h1 className="font-bold text-4xl text-center">Sejarah Kami</h1>
           <br />
           <p className="text-justify">
-            Aute ullamco ex dolore nostrud consequat pariatur anim reprehenderit
-            et ad. Ad laborum non mollit esse Lorem est enim tempor sunt.
-            Eiusmod voluptate ex occaecat commodo. Adipisicing nulla consequat
-            reprehenderit cillum. Ad dolor elit in ullamco enim. Excepteur
-            veniam laboris in tempor laboris dolore excepteur. Cupidatat
-            occaecat irure cillum id exercitation ut nulla. Quis velit sint sint
-            et elit sint aliquip non qui. Minim in eiusmod excepteur officia
-            laboris adipisicing in. Est dolor nostrud quis minim ad ea ad ex.
-            Amet tempor ea consequat nostrud veniam ea laboris ipsum ullamco ad
-            anim. Veniam proident anim ex et excepteur ea consequat consectetur
-            qui exercitation culpa consectetur. Occaecat veniam voluptate
-            pariatur officia. Elit sit quis consequat officia id dolore aute.
-            Adipisicing anim aliquip anim occaecat qui mollit. Consectetur
-            mollit aliqua eu tempor et eu amet in consectetur laboris. Anim
-            consectetur reprehenderit ut cupidatat irure anim. Ea consequat in
-            eiusmod dolor veniam. Do ipsum sit ad reprehenderit nulla. Aliqua
-            dolore sit consectetur veniam qui fugiat. Eiusmod culpa incididunt
-            ipsum exercitation voluptate in id aute quis. Ullamco nisi eu sunt
-            ut nulla culpa non nulla. Ipsum et aliqua dolor nisi exercitation
-            minim qui non Lorem ea. Laboris ullamco commodo ut exercitation quis
-            commodo nisi magna cupidatat dolor labore in amet. Ipsum incididunt
-            anim sit commodo et. Sint reprehenderit aliquip fugiat enim labore.
-            Laboris exercitation dolore magna consequat deserunt qui consectetur
-            consequat. Magna culpa eu magna quis nulla et.
+            {parse(tentang.sejarah)}
           </p>
         </div>
         <div className="mb-8">
           <h1 className="font-bold text-4xl text-center">Visi</h1>
           <br />
-          <p className="text-justify">
-            Mencetak kader Muhammadiyah yang berakhlakul karimah dan
-            entrepreneur
+          <p className="text-center">
+            {parse(tentang.visi)}
           </p>
         </div>
         <div className="mb-8">
           <h1 className="font-bold text-4xl text-center">Misi</h1>
           <br />
-          <p className="text-justify">
-            Menyelenggarakan pendidiakan yang berbasis pesantren dan
-            entrepreneur
+          <p className="text-center">
+            {parse(tentang.misi)}
           </p>
         </div>
         <div className="mb-8">
           <h1 className="font-bold text-4xl text-center">Tujuan</h1>
           <br />
-          <p className="text-justify">
-            Terbentuknya kader Muhammadiyah yang religius dan entrepreneur
+          <p className="text-center">
+            {parse(tentang.tujuan)}
           </p>
         </div>
       </section>
